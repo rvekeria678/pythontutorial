4c9db0f6245865fb61aaf7b715a9cf6f70397b5e
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-02-09 16:53:40 -0500

    casting tutorial update

---
+++ concepts/casting.py
@@ -0,0 +1,24 @@
+# Python Casting
+
+# Source : https://www.w3schools.com/python/python_casting.asp
+
+# Casting allows for the ability to specify a type on to a variable. It is done using constructor functions
+
+# int() -> constructs integer number from an integer literal, float literal, string literal
+
+# float() -> constructs float number from an integer literal, float literal, string literal
+
+# str() -> constructs string from wide variety of datatypes, including strings, integers, and float literals
+
+x = int(1)      # x will be 1
+y = int(2.7)    # y will be 2
+z = int("3")    # z will be 3
+
+a = float(1)        # a will be 1.0
+b = float(2.3)      # b will be 2.3
+c = float("6")      # c will be 6.0
+d = float("3.3")    # d will be 3.3
+
+e = str("s1")   # e will be 's1'
+f = str("2")    # f will be '2'
+g = str(5.0)    # g will be '5.0'
