46e56512ba2064048bb57458abc147553f8d6121
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-02-09 16:56:55 -0500

    User Input Tutorial Added

---
+++ concepts/userinput.py
@@ -0,0 +1,16 @@
+# Python User Input
+
+# Source: https://www.w3schools.com/python/python_user_input.asp
+
+# Python allows for the ability of user input
+
+# Python 3.6 -> user input() method
+# Python 2.7 -> raw_input() method
+
+# Python 3.6
+username = input("Enter username:")
+print("Username is: " + username)
+
+# Python 2.7
+# username = raw_input("Enter username:")
+# print("Username is: " + username)
