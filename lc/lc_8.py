d5db59845dfc53a5d52fcd7884c81c4c38fa6bb8
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-02-16 13:43:15 -0500

    lc_8 attempt

--- lc/lc_8.py
+++ lc/lc_8.py
@@ -4,6 +4,27 @@
 
 class Solution:
     def myAtoi(self, s: str) -> int:
-        n_flag = False
-        for i in range(len(s)):
+        num = ''
+        r_flag = False
+        upper = 2 ** 31 - 1
+        lower = -2 ** 31
+        for i in s:
+            if not i.isalpha():
+                num += i
+                print(num)
+                r_flag = True
+            elif i.isalpha() and r_flag:
+                num = int(num)
+                if num > upper: 
+                    return upper
+                elif num < lower: 
+                    return lower
+                else: 
+                    return num
+        return 0
             
+s = Solution()
+
+print(s.myAtoi("Hello 1234 World"))
+
+print(s.myAtoi("    -42"))
