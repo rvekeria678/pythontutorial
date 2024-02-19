f7daac6e2db99f6a0edaca2354054033a89544c3
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-02-19 13:37:32 -0500

    lc_3 Solved

--- lc/lc_3.py
+++ lc/lc_3.py
@@ -8,7 +8,6 @@ class Solution:
         longest = 0
         for i in range(len(s)):
             for j in s[i::]:
-                #print('j: "'+j+'"')
                 if j in substr:
                     longest = len(substr) if len(substr) > longest else longest
                     substr = ''
@@ -17,15 +16,36 @@ class Solution:
                     substr += j
         return 1 if len(s) == 1 else longest
 
+# Sliding Window Solution
+class Solution2:
+    def lengthofLongestSubstring(self, s: str) -> int:
+        left, right, max = 0,1,0
+
+        if len(s) < 2:
+            return len(s)
+
+        while right < len(s):
+            if s[right] not in s[left:right]:
+                right += 1
+                if right == len(s):
+                    max = len(s[left:right]) if len(s[left:right]) > max else max
+            else:
+                max = len(s[left:right]) if len(s[left:right]) > max else max
+                left += 1
+
+        return max
 
 s = Solution()
+s2 = Solution2()
 
-print(s.lengthofLongestSubstring('abcabcbb'))
+print(s2.lengthofLongestSubstring('abcabcbb'))
 
-print(s.lengthofLongestSubstring('bbbbb'))
+print(s2.lengthofLongestSubstring('bbbbb'))
 
-print(s.lengthofLongestSubstring('pwwkew'))
+print(s2.lengthofLongestSubstring('pwwkew'))
 
-print(s.lengthofLongestSubstring(' '))
+print(s2.lengthofLongestSubstring(' '))
 
-print(s.lengthofLongestSubstring('h'))
+print(s2.lengthofLongestSubstring('h'))
+
+print(s2.lengthofLongestSubstring('au'))
