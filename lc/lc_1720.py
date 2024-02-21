df5e22c47c0db32092c81a7d29ee7b25a2a3a36f
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-02-21 14:25:11 -0500

    Solved

---
+++ lc/lc_1720.py
@@ -0,0 +1,20 @@
+# Leetcode Problem #1720: Decode XORed Array
+
+# Description: There is a hidden integer array arr that consists of n non-negative integers. It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3]. You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0]. Return the original array arr. It can be proved that the answer exists and is unique.
+
+class Solution:
+    def decode(self, encoded: list[int], first: int) -> list[int]:
+        original = [first] 
+        k = first
+        for i in encoded:
+            k ^= i
+            original.append(k)
+        return original
+            
+s = Solution()
+
+l1 = [1,2,3]
+l2 = [6,2,7,3]
+
+print(s.decode(l1, 1))
+print(s.decode(l2, 4))
