df2d02dc558e181e5b7da7746dc3c72bdd26ba69
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-02-22 14:19:29 -0500

    Solved

---
+++ lc/lc_1732.py
@@ -0,0 +1,20 @@
+# Leetcode Problem #1732: Find the Highest Altitude
+
+# Description: There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+class Solution:
+    def largestAltitude(self, gain: list[int]) -> int:
+        alt, max = 0, 0
+        for i in gain:
+            alt += i
+            max = alt if alt > max else max
+        return max
+
+s = Solution()
+
+gain1 = [-5,1,5,0,-7]
+
+gain2 = [-4,-3,-2,-1,4,3,2]
+
+print(s.largestAltitude(gain1))
+print(s.largestAltitude(gain2))
