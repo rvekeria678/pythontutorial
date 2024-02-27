ad667a74e88cd8df03fcac923a9900f4c0ceaae4
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-02-27 15:48:04 -0500

    In Progress

---
+++ lc/lc_2744.py
@@ -0,0 +1,40 @@
+# Leetcode Problem #2744: Find Maximum Number of String Pairs
+
+# Description: You are given a 0-indexed array words consisting of distinct strings. The string words[i] can be paired with the string words[j] if: The string words[i] is equal to the reversed string of words[j]. 0 <= i < j < words.length. Return the maximum number of pairs that can be formed from the array words. Note that each string can belong in at most one pair.
+
+class Solution:
+    def maximumNumberOfStringPairs(self, words: list[str]) -> int:
+        dict, count = {}, 0
+
+        for i in words:
+            if i[::-1] in words:
+                count += 1
+        return count
+
+#        left, right, count = 0, 0, 0
+
+#        while left <= len(words) - 2:
+#            if right >= len(words):
+#                left += 1
+#                right = left
+#            if words[left] == words[right][::-1]:
+#                count += 1
+#            right += 1
+
+#        return count
+
+s = Solution()
+
+words = ["cd","ac","dc","ca","zz"]
+print(s.maximumNumberOfStringPairs(words))
+
+words = ["ab","ba","cc"]
+print(s.maximumNumberOfStringPairs(words))
+
+words = ["aa","ab"]
+print(s.maximumNumberOfStringPairs(words))
+
+
+
+
+
