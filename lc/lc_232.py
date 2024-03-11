524b5db0be8a8a58a17dfd8db631a9694b35f241
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-03-11 16:11:23 -0400

    In Progress

---
+++ lc/lc_232.py
@@ -0,0 +1,25 @@
+# Leetcode Problem #232: Implement Queue using Stacks
+
+# Description: Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty). Implement the MyQueue class: void push(int x) Pushes element x to the back of the queue, int pop() Removes the element from the front of the queue and returns it, int peek() Returns the element at the front of the queue, boolean empty() Returns true if the queue is empty, false otherwise.
+
+class MyQueue:
+
+    def __init__(self):
+        self.a = []
+        self.b = []
+    def push(self, x: int) -> None:
+        self.a.append(x)
+    def pop(self) -> int:
+        pass
+    def peek(self) -> int:
+        pass
+    def empty(self) -> bool:
+        pass
+
+q = MyQueue()
+
+
+
+
+
+
