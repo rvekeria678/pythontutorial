9afc42e8947cfbc586bfb2515bbdb46710b8fa4f
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-03-12 11:58:27 -0400

    Solved

---
+++ lc/lc_3005.py
@@ -0,0 +1,34 @@
+# Leetcode Problem #3005: Count Elements with Maximum Frequency
+
+# Description: You are given an array nums consisting of positive integers. Return the total frequencies of elements in nums such that those elements all have the maximum frequency. The frequency of an element is the number of occurrences of that element in the array.
+
+class Solution:
+    def maxFrequencyElements(self, nums: list[int]) -> int:
+        frequencies = {}
+        temp = {}
+
+        for num in nums:
+            if num in temp:
+                temp[num] += 1
+                if temp[num] in frequencies:
+                    frequencies[temp[num]] += 1
+                else:
+                    frequencies[temp[num]] = 1
+            else:
+                temp[num] = 1
+                if temp[num] in frequencies:
+                    frequencies[temp[num]] += 1
+                else:
+                    frequencies[temp[num]] = 1
+
+        max_frequency = max(frequencies)
+
+        return max_frequency * frequencies[max_frequency]
+
+s = Solution()
+
+nums = [1,2,2,3,1,4]
+print(s.maxFrequencyElements(nums))
+
+nums = [1,2,3,4,5]
+print(s.maxFrequencyElements(nums))
