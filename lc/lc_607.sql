5295c8034458f81f741f94b1dc5486b8d1c38514
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-03-14 12:52:57 -0400

    In Progress

--- lc/lc_607.sql
+++ lc/lc_607.sql
@@ -4,4 +4,4 @@ SELECT name
 FROM SalesPerson
 LEFT JOIN Orders
 ON Orders.sales_id = SalesPerson.sales_id
-WHERE SalesPerson.sales_id NOT IN (SELECT sales_id FROM Orders WHERE com_id = 1)
+HAVING SalesPerson.sales_id NOT IN (SELECT sales_id FROM Orders WHERE com_id = 1)
