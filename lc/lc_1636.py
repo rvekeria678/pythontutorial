fc86159161bcb2578e50e18c3565cd73b4479058
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-03-18 11:38:37 -0400

    Solved

--- lc/lc_1636.py
+++ lc/lc_1636.py
@@ -5,13 +5,43 @@
 class Solution:
     def frequencySort(self, nums: list[int]) -> list[int]:
         d = {}
+        res = []
         for num in nums:
             if num in d:
                 d[num] += 1
             else:
                 d[num] = 1
-        d.fromkeys()
+        freq = sorted(d, key=lambda x:d[x])
+        for x in freq:
+            temp = [x] * d[x]
+            res += temp
+        return res
 
+class Solution:
+    def frequencySort(self, nums: list[int]) -> list[int]:
+        temp, frequency = {}, {}
+        for num in nums:
+            if num in temp:
+                temp[num] += 1
+            else:
+                temp[num] = 1
+            if temp[num] in frequency:
+                frequency[temp[num]].append(num)
+                if temp[num]-1 in frequency:
+                    frequency[temp[num]-1].remove(num)
+            else:
+                frequency[temp[num]] = [num]
+                if temp[num]-1 in frequency:
+                    frequency[temp[num]-1].remove(num)
+        res = []
+        for freq in frequency:
+            values = sorted(frequency[freq], reverse=True)
+            for val in values:
+                temp = [val] * freq
+                res += temp
+
+        return res
+    
 s = Solution()
 
 nums = [1,1,2,2,2,3]
