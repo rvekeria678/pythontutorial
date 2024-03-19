7ef61078bb66aca021be2dc849c0c39f5cf38df9
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-03-19 14:54:03 -0400

    In Progress

---
+++ lc/lc_705.py
@@ -0,0 +1,31 @@
+# Leetcode Problem #705: Design Hashset
+
+# Description: Design a HashSet without using any built-in hash table libraries. Implement MyHashSet class: void add(key) Inserts the value key into the HashSet. bool contains(key) Returns whether the value key exists in the HashSet or not. void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+class MyHashSet:
+
+    def __init__(self):
+        pass
+
+    def add(self, key: int) -> None:
+        pass
+
+    def remove(self, key: int) -> None:
+        pass
+
+    def contains(self, key: int) -> bool:
+        pass
+
+# Your MyHashSet object will be instantiated and called as such:
+obj = MyHashSet()
+print(obj.add(1))
+print(obj.add(2))
+print(obj.contains(1))
+print(obj.)
+
+print(obj.remove(key))
+param_3 = obj.contains(key)
+        
+
+
+
