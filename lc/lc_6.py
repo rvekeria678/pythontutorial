a81a2715e62b78f78f6c3e65f3cf1da110b39d77
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-03-27 11:26:43 -0400

    Solve -h

--- lc/lc_6.py
+++ lc/lc_6.py
@@ -4,13 +4,16 @@
 
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        ans = ""
-        switch = False
-        grid = [[] * len(numRows)]
-        for i in range(len(s)):
-            if not i % numRows:
-                switch = not switch
-            
+        if numRows == 1: return s
+        res = ''
+        for i in range(numRows):
+            increment = 2 * (numRows - 1)
+            for j in range(i, len(s), increment):
+                res += s[j]
+                if i > 0 and i < numRows - 1 and j + increment - 2 * i < len(s):
+                    res += s[j + increment - 2 * i] 
+        return res
+                    
 
 s = Solution()        
 
