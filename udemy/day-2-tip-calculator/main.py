6ab77eec1b67fa52bdf2ac5ef32186a405b3369c
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-03-28 13:30:34 -0400

    Finished

--- udemy/day-2-tip-calculator/main.py
+++ udemy/day-2-tip-calculator/main.py
@@ -5,7 +5,7 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 party_size = int(input("How many people to split the bill? "))
 
-total_bill = bill + bill * (tip * 1 / 100)
+total_bill = bill + bill * (tip / 100)
 individual_cost = total_bill / party_size
 
 print(f"Each person should pay: ${individual_cost:.2f}")
