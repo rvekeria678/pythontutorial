3d75856cdc9cf71836afdc273474e2766c604600
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-01 15:04:42 -0400

    In Progress

--- udemy/day-5-password-generator/main.py
+++ udemy/day-5-password-generator/main.py
@@ -1,1 +1,16 @@
 # Password Generator
+
+import random
+
+print("Welcome to the PyPassword Generator!")
+print("How many letters would you like in your password?")
+number_of_letters = int(input())
+
+print("How many symbols would you like?")
+number_of_symbols = int(input())
+
+print("How many numbers would you like?")
+number_of_numbers = int(input())
+
+while number_of_letters + number_of_symbols + number_of_numbers:
+    
