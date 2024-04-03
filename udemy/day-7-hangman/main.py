8a267a1c349ee124f2eae0f9e4a26fe1cf7a3070
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-03 12:57:57 -0400

    finished

--- udemy/day-7-hangman/main.py
+++ udemy/day-7-hangman/main.py
@@ -2,42 +2,56 @@
 
 import random
 import hangmanui
-
-word_bank = ['aardvark', "baboon", "camel"]
-
+import dictionary
 
 def driver():
-    # Presets
-    stage = 0
-    chosen_word = random.choice(word_bank)
-    game_complete = False
-    display = ['_'] * len(chosen_word)
-    lives = 5
+    while 1:
+        # Presets
+        stage = 0
+        chosen_word = random.choice(dictionary.dictionary)
+        game_complete = False
+        display = ['_'] * len(chosen_word)
+        lives = 4
+        seen = {}
 
-    # Title Screen
-    print(hangmanui.title)
+        # Title Screen
+        print(hangmanui.title)
 
-    while not game_complete:
-        print(hangmanui.stages[stage])
-        print(' '.join(display))
-        guess = input("Guess a Letter: ")
+        while not game_complete:
+            print(hangmanui.stages[stage])
+            print(' '.join(display))
+            print("Letters Guessed:",' '.join(seen))
+
+            guess = input("Guess a Letter: ")
     
-        for position in range(len(chosen_word)):
-            if guess == chosen_word[position]:
-                display[position] = guess
+            if guess not in seen:
+                seen[guess] = ''
+
+            for position in range(len(chosen_word)):
+                if guess == chosen_word[position]:
+                    display[position] = guess
             
-        if guess not in chosen_word:
-            lives -= 1
-            stage += 1
+            if guess not in chosen_word:
+                lives -= 1
+                stage += 1
         
-        if '_' not in display:
-            print(hangmanui.won_screen)
-            game_complete = True
+            if '_' not in display:
+                print(hangmanui.won_screen)
+                print("Congrats, you won!")
+                game_complete = True
 
-        if lives <= 0:
-            print(hangmanui.lose_screen)
-            game_complete = True
+            if lives <= 0:
+                print(hangmanui.stages[stage])
+                print(' '.join(display))
+                print("Letters Guessed:",' '.join(seen))
+                print(hangmanui.lose_screen)
+                print("Sorry, you lost! Better luck next time.")
+                print("The word was",chosen_word)
+                game_complete = True
 
-
+        print("Would you like to play again? (y = Yes, n = No):")
+        choice = input().lower()
+        if choice != 'y':
+            break
 
 driver()
