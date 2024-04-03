356e9e38cd79952d9bf1a2b1c9b4c4798d3c3d60
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-03 10:27:55 -0400

    Solved

---
+++ lc/lc_3014.py
@@ -0,0 +1,25 @@
+# Leetcode Problem #3014: Minimum Number of Pushes to Type Word I
+
+# Description: You are given a string word containing distinct lowercase English letters. Telephone keypads have keys mapped with distinct collections of lowercase English letters, which can be used to form words by pushing them. For example, the key 2 is mapped with ["a","b","c"], we need to push the key one time to type "a", two times to type "b", and three times to type "c" . It is allowed to remap the keys numbered 2 to 9 to distinct collections of letters. The keys can be remapped to any amount of letters, but each letter must be mapped to exactly one key. You need to find the minimum number of times the keys will be pushed to type the string word. Return the minimum number of pushes needed to type word after remapping the keys. An example mapping of letters to keys on a telephone keypad is given below. Note that 1, *, #, and 0 do not map to any letters.
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        unique_letters = len(set(word))
+        clicks, steps = 0, 1
+        while unique_letters > 8:
+            unique_letters -= 8
+            clicks += 8 * steps
+            steps += 1
+        return clicks + unique_letters * steps
+
+s = Solution()
+
+word = "abcde"
+print(s.minimumPushes(word))
+
+word = "xycdefghij"
+print(s.minimumPushes(word))
+
+word = "acolkxjbizfmhnrdq"
+print(s.minimumPushes(word))
+
