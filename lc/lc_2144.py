2bb1e109d444456e6db3f4ce5e32e7d949b69c4d
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-03 10:58:34 -0400

    In Progress

---
+++ lc/lc_2144.py
@@ -0,0 +1,24 @@
+# Leetcode Problem #2144: Minimum Cost of Buying Candies With Discount
+
+# Description: A shop is selling candies at a discount. For every two candies sold, the shop gives a third candy for free. The customer can choose any candy to take away for free as long as the cost of the chosen candy is less than or equal to the minimum cost of the two candies bought. For example, if there are 4 candies with costs 1, 2, 3, and 4, and the customer buys candies with costs 2 and 3, they can take the candy with cost 1 for free, but not the candy with cost 4. Given a 0-indexed integer array cost, where cost[i] denotes the cost of the ith candy, return the minimum cost of buying all the candies.
+
+class Solution:
+    def minimumCost(self, cost: list[int]) -> int:
+        cost = sorted(cost)
+        total = 0
+        if len(cost) <= 2: return sum(cost)
+        for i in range(len(cost)):
+            if i % 3 != 0:
+                total += cost[i]
+        return total
+
+s = Solution()
+
+cost = [1,2,3]
+print(s.minimumCost(cost))
+
+cost = [6,5,7,9,2,2]
+print(s.minimumCost(cost))
+
+cost = [5,5]
+print(s.minimumCost(cost))
