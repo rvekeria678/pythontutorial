fccd0d301b94a2327e6ed192c7222defa644c266
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-05 12:03:27 -0400

    Finished Tutorial

--- udemy/day-16-start/main.py
+++ udemy/day-16-start/main.py
@@ -1,5 +1,23 @@
-from turtle import Turtle
+#from turtle import Turtle, Screen
 
-timmy = Turtle()
+#timmy = Turtle()
+#print(timmy)
+#timmy.shape('turtle')
+#timmy.color('darkorchid2')
 
-print(timmy)
+#timmy.forward(100)
+
+#my_screen = Screen()
+#print(my_screen.canvheight)
+#my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column('Name', ["Pikachu","Squirtle","Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = 'l'
+
+print(table)
