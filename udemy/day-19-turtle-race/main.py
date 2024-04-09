3d883384fc6633736ce70124bdaca983b94f001d
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-09 13:11:05 -0400

    Progress Save

---
+++ udemy/day-19-turtle-race/main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+
+my_turtle1 = Turtle()
+my_turtle2 = Turtle()
+
+my_turtle1.color('blue')
+my_turtle2.color('red')
+
+my_turtle1.forward(100)
+my_turtle2.back(100)
+
+
+screen = Screen()
+screen.exitonclick()
