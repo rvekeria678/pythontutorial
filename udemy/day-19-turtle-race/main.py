fb6bab08d764420f0d165e54d1eb947d48826778
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-09 13:53:32 -0400

    Progress Finished

--- udemy/day-19-turtle-race/main.py
+++ udemy/day-19-turtle-race/main.py
@@ -1,15 +1,40 @@
 from turtle import Turtle, Screen
-
-
-my_turtle1 = Turtle()
-my_turtle2 = Turtle()
-
-my_turtle1.color('blue')
-my_turtle2.color('red')
-
-my_turtle1.forward(100)
-my_turtle2.back(100)
-
+import random
 
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title='Make you bet',prompt="Which turtle will win the race? Enter a color: ")
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+continue_race = False
+
+turtles = []
+for color in colors:
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.color(color)
+    turtles.append(new_turtle)
+
+start_x = -230
+start_y = -150
+
+for turtle in turtles:
+    turtle.penup()
+    turtle.goto(start_x, start_y)
+    start_y += 60
+
+if user_bet:
+    continue_race = True
+
+while continue_race:
+    for turtle in turtles:
+        turtle.forward(random.randint(0,10))
+
+        if turtle.xcor() >= 220:
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f'You won! The {winning_color} turtle is the winner!')
+            else:
+                print(f"You lost! The {winning_color} turtle is the winner")
+            continue_race = False
+            break
+
 screen.exitonclick()
