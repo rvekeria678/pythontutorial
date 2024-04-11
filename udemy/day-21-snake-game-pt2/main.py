b97d893ebf9843384c66ca856bcb60b17283789d
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-11 10:45:41 -0400

    Progress Save

--- udemy/day-21-snake-game-pt2/main.py
+++ udemy/day-21-snake-game-pt2/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(key='Up', fun=snake.up)
@@ -20,8 +22,10 @@ game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
     
+    # Detect Collision
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
