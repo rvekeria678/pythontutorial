4755e3f688a629fe5257f077942449d119f6f3dd
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-11 12:07:56 -0400

    Progress Save

--- udemy/day-21-snake-game-pt2/main.py
+++ udemy/day-21-snake-game-pt2/main.py
@@ -38,11 +38,10 @@ while game_on:
         game_on = False
 
     # Detect collision with tail
-    for segment in snake.segments:
-        if segment != snake.head:
-            if snake.head.distance(segment) < 10:
-                scoreboard.game_over()
-                game_on = False
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_on = False
 
 
 screen.exitonclick()
