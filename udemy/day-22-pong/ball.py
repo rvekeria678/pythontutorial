5da2a31d4e3ecbed861b6ba8b28eadaf8ca4363b
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-11 15:14:21 -0400

    Progress Save

--- udemy/day-22-pong/ball.py
+++ udemy/day-22-pong/ball.py
@@ -6,10 +6,21 @@ class Ball(Turtle):
         super().__init__()
         self.shape(BALL_SHAPE)
         self.color(BALL_COLOR)
+        self.penup()
         self.shapesize(stretch_wid=BALL_STRETCH_WIDTH, stretch_len=BALL_STRETCH_LENGTH)
         self.goto(x=0,y=0)
+        self.x_vel = 10
+        self.y_vel = 10
     
     def move(self):
-        self.goto(x=self.xcor()+BALL_SPEED,y=self.ycor()+BALL_SPEED)
+        new_x = self.xcor() + self.x_vel
+        new_y = self.ycor() + self.y_vel
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_vel *= -1
+
+    def hit(self):
+        self.x_vel *= -1
         
         
--- udemy/day-22-pong/main.py
+++ udemy/day-22-pong/main.py
@@ -6,7 +6,7 @@ import time
 
 screen = Screen()
 screen.bgcolor('black')
-screen.screensize(canvwidth=SCREEN_SIZE_X, canvheight=SCREEN_SIZE_Y)
+screen.setup(width=SCREEN_SIZE_X, height=SCREEN_SIZE_Y)
 screen.title('Pong')
 screen.tracer(0)
 
@@ -23,9 +23,22 @@ screen.onkeypress(key='s', fun=left_paddle.go_down)
 
 game_on = True
 while game_on:
+    time.sleep(0.08)
     screen.update()
-    time.sleep(0.01)
-    
-    
+    ball.move()    
+
+    # Wall Detection
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    # Paddle Detection
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor()  < -320:
+        ball.hit()
+
+    # Out-of-Bounds Detection
+    if ball.xcor() > 400 or ball.xcor() < -400:
+        ball.goto(0,0)
+        ball.x_vel = 10
+        ball.y_vel = 10
 
 screen.exitonclick()
