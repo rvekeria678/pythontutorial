a7f992e6fe7cc46cda31cd95a0ca43e8f3992649
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-12 13:25:22 -0400

    Progress Save

--- udemy/day-24-snake-highscore/scoreboard.py
+++ udemy/day-24-snake-highscore/scoreboard.py
@@ -2,13 +2,12 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
-
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_highscore()
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -21,8 +20,14 @@ class Scoreboard(Turtle):
 
     def reset(self):
         self.high_score = max(self.score, self.high_score)
+        with open('highscore.txt',mode='w') as file:
+            file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
+
+    def get_highscore(self):
+        with open('highscore.txt') as file:
+            return int(file.read())
 
 #    def game_over(self):
 #        self.goto(0, 0)
