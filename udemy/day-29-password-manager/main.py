90e6d4987ce994b5e3611c9567952c39c7075242
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-16 18:39:40 -0400

    Progress Save

--- udemy/day-29-password-manager/main.py
+++ udemy/day-29-password-manager/main.py
@@ -8,7 +8,9 @@ from tkinter import Tk, Label, Button, Entry, Canvas, PhotoImage
 root = Tk()
 root.title("Password Manager")
 root.config(padx=20, pady=20)
-canvas = Canvas(width=200, height=200, bg='#ffffff')
-
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file=r'udemy\day-29-password-manager\logo.png')
+canvas.create_image(100,100, image=lock_img)
+canvas.pack()
 
 root.mainloop()
