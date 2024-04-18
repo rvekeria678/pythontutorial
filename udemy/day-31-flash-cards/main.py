a767e9ff08684d9d7c2528c14ed9679a657289b0
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-18 15:26:19 -0400

    Progress Save

--- udemy/day-31-flash-cards/main.py
+++ udemy/day-31-flash-cards/main.py
@@ -3,7 +3,7 @@ from dictionary import DATA
 from config import CARD_FRONT_IMG_PATH, CARD_BACK_IMG_PATH, BLACK, WHITE
 import random
 import sys
-
+print(CARD_FRONT_IMG_PATH)
 # ----- CONSTANTS ------------- #
 DATA_SIZE = len(DATA['French'])
 # ----- GLOBALS --------------- #
@@ -13,35 +13,26 @@ selected = -1
 # ----- NEXT CARD LOGIC ------- #
 def next_card():
     global selected
+    global timer
+    flashy.after_cancel(timer)
     selected = random.choice(list(pile))
-    flashy.card_face.itemconfig(flashy.card_image, CARD_FRONT_IMG_PATH)
-    flashy.card_face.itemconfig(flashy.title, text = 'French', fill = BLACK)
-    flashy.word.face.itemconfig(flashy.word, DATA['French'][selected], fill = BLACK)
+    flashy.card_face.itemconfig(flashy.card_image, image=flashy.card_front)
+    flashy.card_face.itemconfig(flashy.title, text = 'French', fill=BLACK)
+    flashy.card_face.itemconfig(flashy.word, text=DATA['French'][selected], fill=BLACK)
+    timer = flashy.after(3000, flip_card)
 
 # ----- FLIP CARD LOGIC ------- #
 def flip_card():
-    flashy.card_face.itemconfig(flashy.card_image, CARD_FRONT_IMG_PATH)
-    flashy.card_face.itemconfig(flashy.title, text = 'English', fill = WHITE)
-    flashy.word.face.itemconfig(flashy.word, DATA['English'][selected], fill = WHITE)
+    flashy.card_face.itemconfig(flashy.card_image, image=flashy.card_back)
+    flashy.card_face.itemconfig(flashy.title, text='English', fill=WHITE)
+    flashy.card_face.itemconfig(flashy.word, text=DATA['English'][selected], fill=WHITE)
 # ----- CLICKED LOGIC --------- #
 def clicked(event, correct):
     global incorrect
-    
     if not correct: incorrect += 1
     else: pile.discard(selected)
-    
     if not pile: sys.exit(0)
-    
     next_card()
-
-# ----- ENABLE BUTTONS -------- #
-def enable_buttons():
-    flashy.right.bind('<Button-1>',func=lambda event: clicked(event, 1))
-    flashy.wrong.bind('<Button-1>',func=lambda event: clicked(event, 0))
-# ----- DISABLE BUTTONS ------- #
-def disable_buttons():
-    flashy.right.unbind('<Button-1>')
-    flashy.wrong.unbind('<Button-1>')
 # ----- USER INTERFACE -------- #
 flashy = Flashy_UI()
 
@@ -49,6 +40,11 @@ flashy = Flashy_UI()
 flashy.right.bind('<Button-1>',func=lambda event: clicked(event, 1))
 flashy.wrong.bind('<Button-1>',func=lambda event: clicked(event, 0))
 
-flashy.wrong.event_generate('<Button-1>')
+timer = flashy.after(3000, flip_card)
+
+selected = random.choice(list(pile))
+flashy.card_face.itemconfig(flashy.card_image, image=flashy.card_front)
+flashy.card_face.itemconfig(flashy.title, text = 'French', fill=BLACK)
+flashy.card_face.itemconfig(flashy.word, text=DATA['French'][selected], fill=BLACK)
 
 flashy.mainloop()
