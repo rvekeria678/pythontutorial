be6f6b717e2fa7f862045cf62b588857fea028b3
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-19 15:25:48 -0400

    Save Progress

--- udemy/day-32-automated-birthday-email/main.py
+++ udemy/day-32-automated-birthday-email/main.py
@@ -1,5 +1,10 @@
 import smtplib
 
 my_email = "zeffarlo53@gmail.com"
+password = "kgck smtk dshj nbka"
+sender_email = "farlotaylor@yahoo.com"
 
-connection = smtplib.SMTP("smptb.gmail.com")
+with smtplib.SMTP("smtp.gmail.com", 587, timeout=120) as connection:
+    connection.starttls()
+    connection.login(user=my_email,password=password)
+    connection.sendmail(from_addr=my_email, to_addrs=sender_email, msg="Subject:hello\n\nThis is the body of my email.")
