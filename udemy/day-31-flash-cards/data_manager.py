0f15287d1bc1a27a9c501accafad4c19b98448af
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-19 12:47:40 -0400

    Progress Save

--- udemy/day-31-flash-cards/data_manager.py
+++ udemy/day-31-flash-cards/data_manager.py
@@ -8,27 +8,43 @@ class Data_Manager:
             self.all_data()
         else:
             try:
-                self.data = pandas.read_json(TO_LEARN_PATH).to_json()
-            except:
+                self.data = pandas.read_json(TO_LEARN_PATH).to_dict()
+                if not self.data['French']:
+                    self.clear_data()
+                    self.all_data()
+            except FileNotFoundError:
                 print("Failed to load resources.")
                 sys.exit(0)
 
-    def delete_data(self, index):
-        pass
+    def clear_data(self):
+        with open(TO_LEARN_PATH, 'w') as data_file:
+            data_file.write('')
 
-    def save_data(self, index):
+    def delete_data(self, index):
+        if index in self.data['French'] and index in self.data['English']:
+            self.data['French'].pop(index)
+            self.data['English'].pop(index)
+        else:
+            print("Data has been corrupted. Closing application to reset save data.")
+            self.clear_data()
+            sys.exit(1)        
+        self.save_data()
+
+    def save_data(self):
         with open(TO_LEARN_PATH, 'w') as data_file:
             json.dump(self.data, data_file)
 
     def all_data(self):
         try:
-            self.data = pandas.read_csv(DICTIONARY_PATH).to_json()
+            self.data = pandas.read_csv(DICTIONARY_PATH).to_dict()
         except FileNotFoundError:
             print("Failed to load resources.")
             sys.exit(0)
         finally:
             self.save_data()
 
-data_manager = Data_Manager()
-
-
+    def data_size(self):
+        return min(len(self.data['French']), len(self.data['English']))
+    
+    def data_keys(self):
+        return list(self.data['French'].keys())
--- udemy/day-31-flash-cards/main.py
+++ udemy/day-31-flash-cards/main.py
@@ -1,37 +1,45 @@
 from flashy_interface import Flashy_UI
-from dictionary import DATA
+from data_manager import Data_Manager
 from config import CARD_FRONT_IMG_PATH, CARD_BACK_IMG_PATH, BLACK, WHITE
 import random
 import sys
-print(CARD_FRONT_IMG_PATH)
+flashy_data = Data_Manager()
 # ----- CONSTANTS ------------- #
-DATA_SIZE = len(DATA['French'])
+DATA_SIZE = flashy_data.data_size()
 # ----- GLOBALS --------------- #
 incorrect = 0
-pile = {i for i in range(DATA_SIZE)}
-selected = -1
+seen = []
+card_count = 0
 # ----- NEXT CARD LOGIC ------- #
 def next_card():
     global selected
     global timer
     flashy.after_cancel(timer)
-    selected = random.choice(list(pile))
+    selected = random.choice(flashy_data.data_keys())
+    while selected in seen:
+        selected = random.choice(flashy_data.data_keys())
+    seen.append(selected)
     flashy.card_face.itemconfig(flashy.card_image, image=flashy.card_front)
     flashy.card_face.itemconfig(flashy.title, text = 'French', fill=BLACK)
-    flashy.card_face.itemconfig(flashy.word, text=DATA['French'][selected], fill=BLACK)
+    flashy.card_face.itemconfig(flashy.word, text=flashy_data.data['French'][selected], fill=BLACK)
     timer = flashy.after(3000, flip_card)
 
 # ----- FLIP CARD LOGIC ------- #
 def flip_card():
     flashy.card_face.itemconfig(flashy.card_image, image=flashy.card_back)
     flashy.card_face.itemconfig(flashy.title, text='English', fill=WHITE)
-    flashy.card_face.itemconfig(flashy.word, text=DATA['English'][selected], fill=WHITE)
+    flashy.card_face.itemconfig(flashy.word, text=flashy_data.data['English'][selected], fill=WHITE)
 # ----- CLICKED LOGIC --------- #
 def clicked(event, correct):
     global incorrect
+    global selected
+    global card_count
+    card_count += 1
     if not correct: incorrect += 1
-    else: pile.discard(selected)
-    if not pile: sys.exit(0)
+    else: flashy_data.delete_data(selected)
+    if card_count >= DATA_SIZE: 
+        print("Finished")
+        sys.exit(0)
     next_card()
 # ----- USER INTERFACE -------- #
 flashy = Flashy_UI()
@@ -42,9 +50,13 @@ flashy.wrong.bind('<Button-1>',func=lambda event: clicked(event, 0))
 
 timer = flashy.after(3000, flip_card)
 
-selected = random.choice(list(pile))
+selected = random.choice(flashy_data.data_keys())
+while selected in seen:
+    selected = random.choice(flashy_data.data_keys())
+seen.append(selected)
+
 flashy.card_face.itemconfig(flashy.card_image, image=flashy.card_front)
 flashy.card_face.itemconfig(flashy.title, text = 'French', fill=BLACK)
-flashy.card_face.itemconfig(flashy.word, text=DATA['French'][selected], fill=BLACK)
+flashy.card_face.itemconfig(flashy.word, text=flashy_data.data['French'][selected], fill=BLACK)
 
 flashy.mainloop()
