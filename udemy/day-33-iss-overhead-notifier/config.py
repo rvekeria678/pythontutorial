36c2f49b45fc7ce75fbd82ae9336f74882838fe5
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-22 13:01:24 -0400

    Progress Save

---
+++ udemy/day-33-iss-overhead-notifier/config.py
@@ -0,0 +1,8 @@
+ISS_URL = "http://api.open-notify.org/iss-now.json"
+SS_URL = "https://api.sunrise-sunset.org/json"
+MARGIN = 5
+
+EMAIL = "zeffarlo53@gmail.com"
+PASSWORD = "kgck smtk dshj nbka"
+PORT = 587
+TIMEOUT = 120
--- udemy/day-33-iss-overhead-notifier/main.py
+++ udemy/day-33-iss-overhead-notifier/main.py
@@ -1,37 +1,46 @@
-import requests
+import requests, time, smtplib
 from datetime import datetime
+from config import ISS_URL, SS_URL, MARGIN, PORT, TIMEOUT, EMAIL, PASSWORD
+import geocoder
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+def current_loc():
+    location = geocoder.ip('me')
+    return (location.latlng[0], location.latlng[1])
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
+def iss_loc():
+    response = requests.get(ISS_URL)
+    response.raise_for_status()
+    data = response.json()
+    return (float(data['iss_position']['latitude']), float(data['iss_position']['longitude']))
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
+def dark(latitude, longitude):
+    parameters = {'lat': latitude, 'lng':longitude, 'formatted':0}
+    response = requests.get(SS_URL,params=parameters)
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    
+    time_now = datetime.now().hour
+    return True if time_now < sunrise or time_now > sunset else False
 
-#Your position is within +5 or -5 degrees of the ISS position.
+def visible(lat, lng, isslat, isslng):
+    if abs(lat-isslat) <=MARGIN and abs(lng-isslng) <=MARGIN and dark(lat,lng):
+        return True
+    return False
 
+# LOGIC
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+CUR_LAT, CUR_LNG = current_loc()
+iss_lat, iss_lng = iss_loc()
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-time_now = datetime.now()
-
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+while visible(CUR_LAT, CUR_LNG, iss_lat, iss_lng):
+    print("Sending Notification...")
+    with smtplib.SMTP('smtp.gmail.com', port=PORT, timeout=TIMEOUT) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL, to_addrs=EMAIL, msg='Subject:ISS NOTIFIER\n\nLook Up, there is a good chance you will see the ISS in the sky.')
+    time.sleep(60)
+    iss_lat, iss_lng = iss_loc()
 
 
 
