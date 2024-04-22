a0fc1c24bcc380723e5269912b83007570993fba
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-22 12:09:53 -0400

    Progress Save

--- udemy/day-33-start/sunset.py
+++ udemy/day-33-start/sunset.py
@@ -1,4 +1,5 @@
 import requests, geocoder
+from datetime import datetime
 
 location = geocoder.ip('me')
 
@@ -8,12 +9,19 @@ CUR_LONG = location.latlng[1]
 API_PATH = 'https://api.sunrise-sunset.org/json'
 
 PARAMETERS = {'lat': CUR_LAT, 
-              'lng': CUR_LONG}
+              'lng': CUR_LONG,
+              'formatted':0}
 
 response = requests.get(API_PATH, params=PARAMETERS)
 response.raise_for_status()
 
 data = response.json()
 
-print(data)
+sunrise = int(data['results']['sunrise'].split('T')[1].split(':')[0])
+sunset = int(data['results']['sunset'].split('T')[1].split(':')[0])
 
+print(sunrise, sunset)
+
+time_now = datetime.now().hour
+
+print(time_now)
