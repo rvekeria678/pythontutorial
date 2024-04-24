ef59652f7bf473235b8b992b85d35fa7049f3224
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-24 10:46:11 -0400

    Progress Save

--- udemy/day-37-habbit-tracker/main.py
+++ udemy/day-37-habbit-tracker/main.py
@@ -7,9 +7,24 @@ pixela_endpoint = 'https://pixe.la/v1/users'
 
 user_params = {
     "token": os.environ['TOKEN'],
-    "username": os.environ['USERNAME'],
+    "username": os.environ['PIXELA_USERNAME'],
     "agreeTermsOfService": "yes",
-    "notMinor": "no"
+    "notMinor": "yes"
 }
 
-requests.post()
+#response = requests.post(url=pixela_endpoint, json=user_params)
+#print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{os.environ["PIXELA_USERNAME"]}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+response = requests.post(url=graph_endpoint,json=graph_config, headers=os.environ['TOKEN'])
+
+print(response.text)
