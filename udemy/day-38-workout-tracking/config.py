6e577771c45ff0d63617612a1d2f5a81ab283c84
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-04-25 11:24:26 -0400

    Progress Save

--- udemy/day-38-workout-tracking/config.py
+++ udemy/day-38-workout-tracking/config.py
@@ -6,5 +6,12 @@ load_dotenv()
 APP_ID = os.environ['APP_ID']
 API_KEY = os.environ['API_KEY']
 
-HOST = 'https://trackapi.nutritionix.com'
-NLE_EP = '/v2/natural/exercise'
+SHEETY_USERNAME = os.environ['SHEETY_USERNAME']
+SHEETY_PROJECTNAME = os.environ['SHEETY_PROJECTNAME']
+SHEETY_SHEETNAME = os.environ['SHEETY_SHEETNAME']
+BEARER_TOKEN = os.environ['BEARER_TOKEN']
+
+NN_HOST = 'https://trackapi.nutritionix.com'
+NN_NLE_EP = '/v2/natural/exercise'
+
+SHEETY_WORKOUT_EP = f"https://api.sheety.co/{SHEETY_USERNAME}/{SHEETY_PROJECTNAME}/{SHEETY_SHEETNAME}"
--- udemy/day-38-workout-tracking/main.py
+++ udemy/day-38-workout-tracking/main.py
@@ -1,20 +1,39 @@
-from config import APP_ID, API_KEY, HOST, NLE_EP
+from config import APP_ID, API_KEY, NN_HOST, NN_NLE_EP, SHEETY_WORKOUT_EP, BEARER_TOKEN
+from datetime import datetime
 import requests
 
-parameters = {
-    "query":"I ran 4 miles and biked 15"
-}
+def get_exercise_data():
+    parameters = {"query": input("Tell me which exercises you did: ")}
+    header = {
+        'x-app-id':APP_ID,
+        'x-app-key':API_KEY
+    }
+    nle_endpoint = f"{NN_HOST}{NN_NLE_EP}"
+    response = requests.post(url=nle_endpoint,json=parameters,headers=header)
+    response.raise_for_status()
+    return response.json()
 
-header = {
-    'x-app-id':APP_ID,
-    'x-app-key':API_KEY
-}
+def upload_to_google_sheets(data):
+    headers = {
+        "Authorization":f"Bearer {BEARER_TOKEN}"
+    }
+    for exercise in data:
+        json_data = {
+            "workout": {
+                "date": datetime.now().strftime("%d/%m/%Y"),
+                "time": datetime.now().isoformat(),
+                "exercise": exercise['name'],
+                "duration": str(exercise['duration_min']),
+                "calories": str(exercise['nf_calories'])
+            }
+        }
 
-nle_endpoint = f"{HOST}{NLE_EP}"
-print(nle_endpoint)
+        response = requests.post(url=SHEETY_WORKOUT_EP, json=json_data, headers=headers)
+        response.raise_for_status()
 
-response = requests.post(url=nle_endpoint,json=parameters,headers=header)
-response.raise_for_status()
 
-data = response.json()
-print(data)
+data = get_exercise_data()
+
+if data['exercises']:
+    upload_to_google_sheets(data['exercises'])
+
