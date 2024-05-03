48658aa6def61f01954ecd621137c8257823eeff
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-05-03 16:34:38 -0400

    Progress Save

--- udemy/day-45-100-must-watch/main.py
+++ udemy/day-45-100-must-watch/main.py
@@ -1,9 +1,14 @@
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 import lxml
 import requests
+import os
 
+load_dotenv()
 
-response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
 response.raise_for_status()
 movies_web_page = response.text
 
@@ -12,5 +17,7 @@ soup = BeautifulSoup(movies_web_page, 'html.parser')
 article_tags = soup.find_all(name='h3', class_='title')
 movie_titles = [el.text for el in article_tags][::-1]
 
-with open('movies.txt',"w", encoding='utf-8') as f:
+with open(os.environ.get("FILEPATH"),mode="w", encoding='utf-8') as f:
+    print("[FILE WRITE] -> movies.txt")
     for title in movie_titles: f.write(f'{title}\n')
+    print("[FILE CLOSE] -> movies.txt")
---
+++ udemy/day-46-spotify-playlist/main.py
@@ -0,0 +1,4 @@
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+
