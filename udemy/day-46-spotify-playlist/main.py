af9f685b7903a7935123919495141a108a80476b
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-05-03 16:34:56 -0400

    Progress Save

--- udemy/day-46-spotify-playlist/main.py
+++ udemy/day-46-spotify-playlist/main.py
@@ -2,3 +2,7 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 
+URL = 'https://www.billboard.com/charts/hot-100/2000-08-12'
+
+input_date = input("Which year do you want to travel to? Type the date in the format YYYY-MM-DD:")
+
