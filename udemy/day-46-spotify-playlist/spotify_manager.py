de2d1b69eecce82630bf1ad3f8367d6dd01498f4
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-05-07 12:03:16 -0400

    Progress Save

--- udemy/day-46-spotify-playlist/spotify_manager.py
+++ udemy/day-46-spotify-playlist/spotify_manager.py
@@ -2,28 +2,34 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 from config.spotipy import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI
 import random
+import datetime
 
 class SpotifyManager:
-    def __init__(self, data) -> None:
-        scope = 'user-library-read'
+    def __init__(self, playlist_data: tuple) -> None:
+        scope = 'playlist-modify-private'
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
                                                        client_secret=SPOTIFY_CLIENT_SECRET,
                                                        redirect_uri=SPOTIFY_REDIRECT_URI,
                                                        scope=scope))
-        
         try:
-            #results = sp.current_user_saved_tracks()
-            
-            #for idx, item in enumerate(results['items']):
-            #    track = item['track']
-            #    print(idx, track['artists'][0]['name'], " – ", track['name'])
+            user = sp.current_user()
+            user_id = user['id']
 
-            play_list = f'R-{random.randint(200,1000)}'
+            playlist_name = f'Top Hits [{playlist_data[0]}]'
+            existing_playlists = sp.user_playlist(user_id)
+            playlist_exists = any(playlist['name'] == playlist_name for playlist in existing_playlists['items'])
 
+            if not playlist_exists:
+                playlist_description = "A playlist created with Spotipy"
+                playlist = sp.user_playlist_create(user_id,
+                                               playlist_name,
+                                               public=False,
+                                               description=playlist_description)
+                print("Playlist Created!")
+            else:
+                print("Playlist Creation Ignored. It Already Exists!")
         except spotipy.exceptions.SpotifyException:
             print("Authentication failed. Check you credentials.")
         
         
         results = sp.current_user
-
-SpotifyManager(1)
