12cf5e8cd9e54f89d616248560d45b43cd6d001c
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-05-14 13:04:40 -0400

    Progress Save

--- udemy/day-48-selenium/cookieclicker.py
+++ udemy/day-48-selenium/cookieclicker.py
@@ -1,48 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
+#-----Constants-----#
 URL = "https://orteil.dashnet.org/experiments/cookie/"
+#-----Globals-----#
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
+
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 
+cookie = driver.find_element(By.ID, value='cookie')
+
 timeout = time.time() + 5
-
-def buy_most_expensive():
-    try:
-        store = driver.find_element(By.ID, value="store")
-        products = store.find_elements(By.TAG_NAME, value='div')[:-1]    
-        options = [product.find_element(By.TAG_NAME, value='b').text.split(' - ') for product in products]
-        product_names = [option[0] for option in options]
-        product_prices = [int(option[1].replace(',','')) for option in options]
-    
-        product_mapper = dict(zip(product_prices, products))
-        price_mapper = dict(zip(product_prices, product_names))
-        print(price_mapper)
-        # Most Expensive Purchase
-        mep = 0
-        money = int(driver.find_element(By.ID, value='money').text.replace(',',''))
-        for price in product_prices:
-            if money >= price and price > mep:
-                mep = price
-    
-        print(f"Purchasing {price_mapper[mep]} for {mep}")
-        product_mapper[mep].click()
-    except Exception as e:
-        print("An error occurred while purchasing:", e)
-
+#-----Cookie Bot Logic-----#
 while True:
-    time.sleep(0.5)
-    while True:
-        if time.time() > timeout:
-            timeout = time.time() + 5
-            break
-        driver.find_element(By.ID, value='cookie').click()
-    
-    buy_most_expensive()
-    
-    cps = driver.find_element(By.ID, value='cps').text
-    print(f"cookies/seconds: {cps}")
+    cookie.click()
+
+    if time.time() > timeout:
+        upgrades = driver.find_elements(By.CSS_SELECTOR, value = "#store div")[:-1]
+
+        bel_text = [el.text for el in driver.find_elements(By.CSS_SELECTOR, value="#store b")][:-1]
+        prices = [int(text.split('-')[1].replace(',','')) for text in bel_text]
+
+        price_bind_div = dict(zip(prices, upgrades))
+
+        money = int(driver.find_element(By.ID, value='money').text.replace(',',''))
+
+        affordable_upgrades = [cost for cost in prices if cost <= money]
+
+        print(f"Money: {money}")
+        print(f"Affordable Upgrades: {affordable_upgrades}")
+
+        if affordable_upgrades:
+            most_expensive_upgrade = max(affordable_upgrades)
+            print(f"MEU: {most_expensive_upgrade}")
+            
+            price_bind_div[most_expensive_upgrade].click()       
+
+        timeout = time.time() + 5
