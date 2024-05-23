54c49a7253345515a344fa6e16d88fad5bc46d6d
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-05-23 14:15:01 -0400

    Progress Save

--- udemy/day-55-start/authsim.py
+++ udemy/day-55-start/authsim.py
@@ -4,16 +4,17 @@ class User:
         self.name = name
         self.is_logged_in = False
 
-    def is_authenticated_decorator(func):
-        def wrapper(*args, **kwargs):
-            if args[0].is_logged_in == True:
-                func(args[0])
-        return wrapper()
+def is_authenticated_decorator(func):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            func(args[0])
+        else: print("Not Authenticated.")
+    return wrapper()
     
-    @is_authenticated_decorator
-    def create_blog_post(user):
-        print(f"This is {user.name}'s new blog post.")
-
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
 
 new_user = User("Sam")
-User.create_blog_post(new_user)
+#new_user.is_logged_in = True
+create_blog_post(new_user)
--- udemy/day-55-start/main.py
+++ udemy/day-55-start/main.py
@@ -2,6 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}<b>"
+    return wrapper
+
+def make_emphasis():
+    pass
+
+def make_underlined():
+    pass
+
 @app.route('/')
 def hello_world():
     return "<h1 style='text-align:center'>Hello, World!</h1>"
