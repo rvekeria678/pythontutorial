eef95c06e8bd0ee53c08e4d7aab6035da49ebf50
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-05-23 15:21:07 -0400

    Save Progress

--- udemy/day-55-start/main.py
+++ udemy/day-55-start/main.py
@@ -7,17 +7,24 @@ def make_bold(function):
         return f"<b>{function()}<b>"
     return wrapper
 
-def make_emphasis():
-    pass
+def make_emphasis(function):
+    def wrapper():
+        return f"<em>{function()}<em>"
+    return wrapper
 
-def make_underlined():
-    pass
+def make_underlined(function):
+    def wrapper():
+        return f"<u>{function()}</u>"
+    return wrapper
 
 @app.route('/')
 def hello_world():
-    return "<h1 style='text-align:center'>Hello, World!</h1>"
+    return "<p style='text-align:center'>Hello, World!</p>"
 
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
 def bye():
     return "Bye!"
 
