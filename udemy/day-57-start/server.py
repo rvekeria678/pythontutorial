3b608e99b4955897bb5fa25337c9f6c54fca948f
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-05-30 10:48:31 -0400

    Progress Save

--- udemy/day-57-start/server.py
+++ udemy/day-57-start/server.py
@@ -24,5 +24,12 @@ def guess(name: str):
 
     return render_template("guess.html", name=name.title(), gender=genderize_data['gender'], age=agify_data['age'])
 
+@app.route('/blog')
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
 if __name__ == "__main__":
     app.run(debug=True)
