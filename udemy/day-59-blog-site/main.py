cf8409b2f35695a450061006b508462a6f29d1be
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-03 14:44:11 -0400

    Progress Save

--- udemy/day-59-blog-site/main.py
+++ udemy/day-59-blog-site/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import requests
 
 #----Constants-----#
-posts_url = 'https://api.npoint.io/674f5423f73deab1e9a7'
+posts_url = 'https://api.npoint.io/55f977f8b55481b3e6dd'
 
 app = Flask(__name__)
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def root():
     response = requests.get(posts_url)
     all_posts = response.json()
+    print(all_posts)
     return render_template('index.html', all_posts=all_posts)
 
 @app.route('/about')
@@ -21,8 +22,10 @@ def contact():
     return render_template('contact.html')
 
 @app.route('/post/<id>')
-def get_post(id: int):
-    pass
+def get_post(id):
+    response = requests.get(posts_url)
+    all_posts = response.json()
+    return render_template('post.html', posts=all_posts, post_id=int(id))
 
 if __name__ == '__main__':
     app.run(debug=True)
