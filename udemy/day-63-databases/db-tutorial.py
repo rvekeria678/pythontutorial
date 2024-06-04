c4c221ffb615273f6100156db098923f13df6ee8
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-04 15:56:43 -0400

    Progress Save

--- udemy/day-63-databases/db-tutorial.py
+++ udemy/day-63-databases/db-tutorial.py
@@ -1,3 +1,7 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 import sqlite3
 
 db = sqlite3.connect("books-collection.db")
@@ -6,5 +10,5 @@ cursor = db.cursor()
 
 #cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+#cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+#db.commit()
