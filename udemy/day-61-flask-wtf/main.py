38d2e1c8209938e1fce09d56753801feb35702b8
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-04 12:05:58 -0400

    Progress Save

--- udemy/day-61-flask-wtf/main.py
+++ udemy/day-61-flask-wtf/main.py
@@ -1,26 +1,29 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from dotenv import load_dotenv
+import os
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
+#----Constants----#
+load_dotenv()
+#----Globals----#
+class LoginForm(FlaskForm):
+    email = StringField(label='Email')
+    pswd = PasswordField(label='Password')
+    sbmt = SubmitField(label="Log In")
 
 app = Flask(__name__)
-
+app.secret_key = os.environ.get('SECRET_KEY')
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
