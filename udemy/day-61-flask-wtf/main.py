d8427632867ed5b3855a6ea7bf250d80141d5cc3
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-04 12:53:03 -0400

    Progress Save

--- udemy/day-61-flask-wtf/main.py
+++ udemy/day-61-flask-wtf/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 from dotenv import load_dotenv
 import os
 
@@ -9,8 +9,8 @@ import os
 load_dotenv()
 #----Globals----#
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
-    pswd = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    pswd = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     sbmt = SubmitField(label="Log In")
 
 app = Flask(__name__)
@@ -23,6 +23,11 @@ def home():
 @app.route('/login', methods=['GET','POST'])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == 'admin@email.com' and login_form.pswd.data == '12345678':
+            return render_template('success.html')
+        else: return render_template('denied.html')
+
     return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
