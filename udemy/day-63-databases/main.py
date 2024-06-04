78dc0c9dbaf770cb44591ac4c2121c53fa151605
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-04 15:56:31 -0400

    Progress Save

--- udemy/day-63-databases/main.py
+++ udemy/day-63-databases/main.py
@@ -7,13 +7,21 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html', books=all_books)
 
 
 @app.route("/add")
 def add():
-    pass
+    return render_template('add.html')
 
+@app.route('/input', methods=['GET','POST'])
+def recieve_data():
+    all_books.append({
+        "title": request.form['title'],
+        "author": request.form['author'],
+        "rating": request.form['rating']
+    })
+    return "You successfull added a book!"
 
 if __name__ == "__main__":
     app.run(debug=True)
