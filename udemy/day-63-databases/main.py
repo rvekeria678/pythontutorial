beabc2f4af6b33b4b13c42906b6c49eb35bafc9e
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-05 16:01:36 -0400

    Progress Save

--- udemy/day-63-databases/main.py
+++ udemy/day-63-databases/main.py
@@ -35,7 +35,7 @@ def home():
     result = db.session.execute(db.select(Book).order_by(Book.title))
     all_books = result.scalars().all()
     return render_template('index.html', books=all_books)
-
+# Add Book to Library
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
@@ -48,10 +48,18 @@ def add():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
-
-@app.route('/edit/<id>', methods=['GET','PUT'])
-def edit(id):
-    return "Some Editing Page"
+# Edit Book Rating
+@app.route('/edit', methods=['GET','POST'])
+def edit():
+    if request.method == 'POST':
+        book_id = request.form['id']
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form['rating']
+        db.session.commit()
+        return redirect(url_for("home"))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit.html", book=book_selected)
 
 #-----Server Start-----#
 if __name__ == "__main__":
