92792616e3ca7c478261461c79311f6b268d3b9c
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-05 15:37:08 -0400

    Progress Save

--- udemy/day-63-databases/db-tutorial.py
+++ udemy/day-63-databases/db-tutorial.py
@@ -6,11 +6,12 @@ import sqlite3
 
 #-----Constants-----#
 DATABASE_URL = "sqlite:///new-books-collection.db"
+#-----Aux-----#
+app = Flask(__name__)
 
 class Base(DeclarativeBase):
     pass
 
-app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
@@ -32,6 +33,31 @@ with app.app_context():
     db.session.add(new_book)
     db.session.commit()
 
+with app.app_context():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalar()
+
+with app.app_context():
+    book = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+    book_to_update.title = "Harry Potter and the Chamber of Secrets"
+    db.session.commit() 
+
+book_id = 1
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_update = db.get_or_404(Book, book_id)  
+    book_to_update.title = "Harry Potter and the Goblet of Fire"
+    db.session.commit()  
+
+book_id = 1
+with app.app_context():
+    book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
 #db = sqlite3.connect("books-collection.db")
 
 #cursor = db.cursor()
--- udemy/day-63-databases/main.py
+++ udemy/day-63-databases/main.py
@@ -1,28 +1,58 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+import sqlite3
 
+#-----Constants-----#
+DATABASE_URL = 'sqlite:///books.db'
+#-----Aux-----#
 app = Flask(__name__)
+class Base(DeclarativeBase):pass
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+#-----Models-----#
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    
+    def as_dict(self):
+        return {
+            col.name: getattr(self, col.name)
+        }
 
-all_books = []
-
-
+    def __repr__(self) -> str:
+        return f'<Book {self.title}>'
+#-----Create Database-----#
+with app.app_context():
+    db.create_all()
+#-----Routes-----#
 @app.route('/')
 def home():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars().all()
     return render_template('index.html', books=all_books)
 
-
-@app.route("/add")
+@app.route('/add', methods=['GET', 'POST'])
 def add():
-    return render_template('add.html')
+    if request.method == 'POST':
+        new_book = Book(
+            title=request.form['title'],
+            author=request.form['author'],
+            rating=request.form['rating']
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("add.html")
 
-@app.route('/input', methods=['GET','POST'])
-def recieve_data():
-    all_books.append({
-        "title": request.form['title'],
-        "author": request.form['author'],
-        "rating": request.form['rating']
-    })
-    return "You successfull added a book!"
+@app.route('/edit/<id>', methods=['GET','PUT'])
+def edit(id):
+    return "Some Editing Page"
 
+#-----Server Start-----#
 if __name__ == "__main__":
     app.run(debug=True)
-
