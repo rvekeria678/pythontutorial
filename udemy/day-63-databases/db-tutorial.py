d9a16a1ab97cf61d993bae4a1ed413638fdf09c8
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-05 14:12:25 -0400

    Progress Save

--- udemy/day-63-databases/db-tutorial.py
+++ udemy/day-63-databases/db-tutorial.py
@@ -4,10 +4,33 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 import sqlite3
 
+#-----Constants-----#
+DATABASE_URL = "sqlite:///new-books-collection.db"
+
 class Base(DeclarativeBase):
     pass
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<Book {self.title}>'
+
+with app.app_context():
+    db.create_all()
+
+with app.app_context():
+    new_book = Book(id=1, title="Harry Potter", author="J.K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
 
 #db = sqlite3.connect("books-collection.db")
 
