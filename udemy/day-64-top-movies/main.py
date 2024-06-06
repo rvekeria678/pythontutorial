795b68aadc9cb40865acb35ee17719416d5328f6
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-06 10:33:17 -0400

    Progress Save

--- udemy/day-64-top-movies/main.py
+++ udemy/day-64-top-movies/main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField, FloatField
 from wtforms.validators import DataRequired
 from dotenv import load_dotenv
 import requests, os
@@ -12,6 +12,8 @@ import requests, os
 #-----Constants-----#
 DATABASE_URL = 'sqlite:///movies.db'
 TMDB_URL = 'https://api.themoviedb.org/3/search/movie'
+TMDB_DETAILS_URL = 'https://api.themoviedb.org/3/movie'
+TMDB_IMG_EP = 'https://image.tmdb.org/t/p/w185'
 load_dotenv()
 #-----Aux-----#
 app = Flask(__name__)
@@ -33,7 +35,7 @@ class Movie(db.Model):
     img_url: Mapped[str] = mapped_column(String, nullable=False)
 #-----Forms-----#
 class EditForm(FlaskForm):
-    rating = IntegerField(label="Your Rating out of 10 e.g. 7.5",
+    rating = FloatField(label="Your Rating out of 10 e.g. 7.5",
                           validators=[DataRequired()])
     review = StringField(label="Your Review",
                          validators=[DataRequired()])
@@ -47,8 +49,16 @@ with app.app_context():
 #-----Routes-----#
 @app.route("/")
 def home():
-    result = db.session.execute(db.select(Movie).order_by(Movie.title))
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating))
     all_movies = result.scalars().all()
+    
+    rank = len(all_movies)
+
+    for movie in all_movies:
+        movie.ranking = rank
+        rank -= 1
+        db.session.commit()
+
     return render_template("index.html", movies=all_movies)
 
 @app.route('/edit', methods=['GET', 'POST'])
@@ -78,21 +88,44 @@ def delete():
 def add():
     add_form = AddForm()
     if add_form.validate_on_submit():
-        tmdb_params = {
-            "query": add_form.movie_title.data,
-            "api_key": os.environ.get('TMDB_API_KEY')
-        }
-        response = requests.get(url=TMDB_URL, params=tmdb_params)
-        results = response.json()
-
-        print(results)
-        #return redirect(url_for('select'))
+        return redirect(url_for('select', query=add_form.movie_title.data))
     return render_template('add.html', form=add_form)
 
-@app.route('/select')
-def select():
+@app.route('/select/<query>')
+def select(query):
+    tmdb_params = {
+        "query": query,
+        "api_key": os.environ.get('TMDB_API_KEY'),
+    }
+    response = requests.get(url=TMDB_URL, params=tmdb_params)
+    results = response.json()
+    return render_template('select.html', data=results)
 
-    return render_template('select.html')
+@app.route('/getfilm')
+def get_film():
+    q_id = request.args.get('film_id')
+    tmdb_params = {
+        "api_key": os.environ.get('TMDB_API_KEY')
+    }
+    response = requests.get(url=f"{TMDB_DETAILS_URL}/{q_id}", params=tmdb_params)
+    result = response.json()
+    print(result)
+
+    new_movie = Movie(
+        id = result['id'],
+        title = result['title'],
+        year = result['release_date'].split('-')[0],
+        description = result['overview'],
+        rating = 0,
+        ranking = 0,
+        review = "None",
+        img_url = f"{TMDB_IMG_EP}{result['poster_path']}"
+    )
+
+    db.session.add(new_movie)
+    db.session.commit()
+
+    return redirect(url_for('edit', id=result['id']))
 
 #-----Server Driver-----#
 if __name__ == '__main__':
