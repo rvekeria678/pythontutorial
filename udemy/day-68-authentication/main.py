2ae4c1fbd9dcaacb8c3a31605c3c1538e14ba491
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-07 12:54:04 -0400

    Progress Save

--- udemy/day-68-authentication/main.py
+++ udemy/day-68-authentication/main.py
@@ -4,18 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-
+from dotenv import load_dotenv
+import os
+#-----Constants-----#
+load_dotenv()
+#-----Aux-----#
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
-
-# CREATE DATABASE
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+#-----Create Database-----#
 class Base(DeclarativeBase):
     pass
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-
-# CREATE TABLE IN DB
+#-----Create Table in Database-----#
 class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
@@ -24,8 +26,10 @@ class User(db.Model):
  
 with app.app_context():
     db.create_all()
-
-
+#-----Create Login Manager-----#
+login_manager = LoginManager()
+login_manager.init_app(app)
+#-----Routes-----#
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -36,7 +40,11 @@ def register():
     if request.method == "POST":
         new_user = User(
             email = request.form['email'],
-            password = request.form['password'],
+            password = generate_password_hash(password=request.form['password'],
+                                              method="pbkdf2:sha256",
+                                              salt_length=8
+            
+            ),
             name = request.form['name']
         )
         db.session.add(new_user)
