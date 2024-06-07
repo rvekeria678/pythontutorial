92d235c70e926b890c6536e9cb5d4794808600e4
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2024-06-06 21:34:43 -0400

    Added Delete Post Operation

--- udemy/day-67-blog-restful/main.py
+++ udemy/day-67-blog-restful/main.py
@@ -100,16 +100,21 @@ def edit_post(post_id):
     return render_template('make-post.html', form=edit_form, edit_mode=True)
 
 # TODO: delete_post() to remove a blog post from the database
+@app.route('/delete/<int:post_id>', methods=['GET','DELETE'])
+def delete_post(post_id):
+    post_to_del = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post_to_del)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-
 @app.route("/contact")
 def contact():
-    return render_template("")
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
